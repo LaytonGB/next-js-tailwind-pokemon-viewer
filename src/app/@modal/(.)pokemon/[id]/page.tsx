c3416322382fa9ae21c3bb76cdf790c84e67c185
Layton <laytonb@hotmail.co.uk>
2024-06-11 08:20:10 +0100

    merge pokemon details pages

--- src/app/@modal/(.)pokemon/[id]/page.tsx
+++ src/app/@modal/(.)pokemon/[id]/page.tsx
@@ -1,20 +1,14 @@
-import { Pokemon } from "@/endpointData";
 import Modal from "@/app/components/Modal";
+import PokemonDetails from "@/app/pokemon/[id]/page";
 
 export default async function PokemonDetailsModal({
   params,
 }: {
   params: { id: string };
 }) {
-  const details = (await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${params.id}`
-  ).then((res) => res.json())) as Pokemon;
-
   return (
     <Modal>
-      <h1 className="text-3xl">
-        {details.name.charAt(0).toUpperCase() + details.name.slice(1)}
-      </h1>
+      <PokemonDetails params={params} />
     </Modal>
   );
 }
