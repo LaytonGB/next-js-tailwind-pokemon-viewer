0ea9a0b491f590922fae74b5cc6b97980764853f
Layton <laytonb@hotmail.co.uk>
2024-06-18 16:18:39 +0100

    remove intentional timeout

--- src/app/@table/page.tsx
+++ src/app/@table/page.tsx
@@ -23,8 +23,6 @@ export default async function PokemonTable({
     )
   );
 
-  await new Promise((resolve) => setTimeout(resolve, 1500));
-
   return (
     <div>
       <table className="m-auto w-full">
